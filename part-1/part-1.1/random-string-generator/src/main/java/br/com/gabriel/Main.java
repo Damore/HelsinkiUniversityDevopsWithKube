cea791231ae9aadf9fadec0a06d0a4d6e93e449a
Gabriel <damore120@gmail.com>
2024-08-12 21:30:43 -0300

    chore: executing and returning the same uuid

--- part-1/part-1.1/random-string-generator/src/main/java/br/com/gabriel/Main.java
+++ part-1/part-1.1/random-string-generator/src/main/java/br/com/gabriel/Main.java
@@ -1,7 +1,14 @@
 package br.com.gabriel;
 
+import java.util.UUID;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        final var uuid = UUID.randomUUID();
+        do {
+            System.out.println(uuid);
+            Thread.sleep(5000);
+        } while (true);
+
     }
 }
